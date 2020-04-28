fa470bc8840f2c4cb218575c098396b6820baa70
tomomasakodama <tomomasa.kodama@force-corp.co.jp>
2020-04-28 18:32:11 +0900

    homepage2

---
+++ homepage1.js
@@ -0,0 +1,8 @@
+'use strict'
+
+const contact = document.getElementById('sample');
+
+contact.addEventListener('click' , () => {
+    // location.href = 'contact.html';
+    console.log("hello world!");
+});
---
+++ js/homepage1.js
@@ -0,0 +1,20 @@
+'use strict'
+
+const contact = document.getElementById('contact');
+const contact1 = document.getElementById('contact1')
+
+contact.addEventListener('click' , () => {
+    location.href = 'contact.html';
+});
+
+contact1.addEventListener('click' , () => {
+    location.href = 'contact.html';
+});
+
+const home = document.getElementById('home');
+
+home.addEventListener('click' , () => {
+    location.href = 'homepage.html';
+});
+
+
